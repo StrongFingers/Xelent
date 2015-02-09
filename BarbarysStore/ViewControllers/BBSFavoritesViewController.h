8c4ac896e6c1768366a853fe9a361e895530925f
Dmitry Kozlov <dmitry@xelentec.com>
2015-02-09 14:09:01 +0300

    add template for other tab bar items view controllers

---
+++ BarbarysStore/ViewControllers/BBSFavoritesViewController.h
@@ -0,0 +1,13 @@
+//
+//  BBSFavoritesViewController.h
+//  BarbarysStore
+//
+//  Created by Dmitry Kozlov on 2/9/15.
+//  Copyright (c) 2015 Xelentec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBSFavoritesViewController : UIViewController
+
+@end
---
+++ BarbarysStore/ViewControllers/BBSShoppingCartViewController.m
@@ -0,0 +1,37 @@
+//
+//  BBSShoppingCartViewController.m
+//  BarbarysStore
+//
+//  Created by Dmitry Kozlov on 2/9/15.
+//  Copyright (c) 2015 Xelentec. All rights reserved.
+//
+
+#import "BBSShoppingCartViewController.h"
+
+@interface BBSShoppingCartViewController ()
+
+@end
+
+@implementation BBSShoppingCartViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
