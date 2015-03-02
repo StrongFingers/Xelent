6d9716ef6051e81dafddb05061e042aba9a9bf71
Dmitry Kozlov <dmitry@xelentec.com>
2015-03-02 15:35:35 +0300

    fix to DB file

--- BarbarysStore/Classes/XLNDatabaseManager.m
+++ BarbarysStore/Classes/XLNDatabaseManager.m
@@ -33,8 +33,8 @@
 
 - (void)createDB {
     if ([[NSFileManager defaultManager] fileExistsAtPath:self.path isDirectory:nil]) {
-        [[NSFileManager defaultManager] removeItemAtPath:self.path error:nil];
-        //return;
+        //[[NSFileManager defaultManager] removeItemAtPath:self.path error:nil];
+        return;
     }
     if (![self.db open]) {
         return;
